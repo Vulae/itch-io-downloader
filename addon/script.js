ff6ae1edac22d1204ce5b29dc864f69eb4cb5586
Vulae <potatocrazy42069@gmail.com>
2024-01-07 19:20:19 -0600

    clean up & icon

--- addon/script.js
+++ addon/script.js
@@ -12,12 +12,16 @@
     const gameId = gameInfo.id;
 
     const buttonsList = document.querySelector('#user_tools');
-    const button = document.createElement('li');
-    buttonsList.appendChild(button);
-    const link = document.createElement('a');
-    button.appendChild(link);
-    link.classList.add('action_btn');
-    link.href = `itch-io-downloader://${gameId}`;
-    link.innerText = 'Download & play this game'
+
+    let button_html = `
+<li>
+    <a class="action_btn" href="itch-io-downloader://${gameId}">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" x2="12" y1="15" y2="3"/></svg>
+        Download & Play
+    </a>
+</li>
+`;
+    let button = new DOMParser().parseFromString(button_html, 'text/html').querySelector('li');
+    buttonsList.append(button);
 
 })();
