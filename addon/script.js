15006dda5b58665340023ff54421995c9fcddeb3
Vulae <potatocrazy42069@gmail.com>
2024-01-26 16:43:51 -0600

    addon script refactor & add button for launcher

--- addon/script.js
+++ addon/script.js
@@ -1,25 +1,88 @@
 
-(async function() {
 
-    if(document.body.getAttribute('data-page_name') != 'view_game') return;
 
+/**
+ * @typedef {object} GameInfo
+ * @property {number} id
+ */
+
+/**
+ * @param {unknown} value
+ * @returns {GameInfo | null}
+ */
+function validateGameInfo(value) {
+    if(typeof value != 'object' || value == null) return null;
+    if(!('id' in value) || typeof value.id != 'number') return null;
+    return value;
+}
+
+/**
+ * @returns {Promise<GameInfo | null>}
+ */
+async function getGameInfo() {
     const gameInfo = await (await fetch(`${location.origin}${location.pathname}/data.json`)).json();
     console.log(gameInfo);
+    return validateGameInfo(gameInfo);
+}
 
-    if(typeof gameInfo != 'object' || gameInfo == null) return;
-    if(!('id' in gameInfo) || typeof gameInfo.id != 'number') return;
 
-    const gameId = gameInfo.id;
 
-    const buttonsList = document.querySelector('#user_tools');
+/**
+ * @param {HTMLDivElement} element #user_tools 
+ * @param {GameInfo} gameInfo 
+ */
+async function displayPlayButton(element, gameInfo) {
 
-    buttonsList.insertAdjacentHTML('beforeend', `
+    element.insertAdjacentHTML('beforeend', `
         <li>
-            <a class="action_btn" href="itch-io-downloader://play/${gameId}">
+            <a class="action_btn" href="itch-io-downloader://play/${gameInfo.id}">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" x2="12" y1="15" y2="3"/></svg>
                 Download & Play
             </a>
         </li>
     `);
 
-})();
+}
+
+
+
+/**
+ * @param {HTMLDivElement} element .primary_header
+ */
+function displayLauncherButton(element) {
+
+    const headerButtons = element.querySelector('.header_buttons');
+
+    headerButtons.insertAdjacentHTML('beforeend', `
+        <a class="header_button" href="itch-io-downloader://play">Launcher</a>
+    `);
+
+}
+
+
+
+async function displayPage() {
+
+    document.querySelectorAll('.primary_header').forEach(element => {
+        displayLauncherButton(element);
+    });
+
+
+
+    if(document.body.getAttribute('data-page_name') == 'view_game') {
+        const gameInfo = await getGameInfo();
+
+        if(!gameInfo) {
+            throw new Error('Failed to get game info.');
+        }
+
+        document.querySelectorAll("#user_tools").forEach(element => {
+            displayPlayButton(element, gameInfo);
+        });
+    }
+
+}
+
+
+
+displayPage();
