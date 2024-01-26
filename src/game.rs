320a1f246e7d1ab223fb0f599b6c3ad959e2d0b2
Vulae <potatocrazy42069@gmail.com>
2024-01-26 17:55:31 -0600

    refactor executable search

--- src/game.rs
+++ src/game.rs
@@ -1,16 +1,10 @@
 #![allow(dead_code)]
 
-use std::{path::PathBuf, error::Error};
+use std::{error::Error, fs::DirEntry, path::PathBuf};
 use serde::{Deserialize, Serialize};
 use tokio::{fs, process::Command};
 
 use crate::{config::Config, api::{itch_api_game_uploads, GameUpload}};
-
-
-
-static SEARCH_BLACKLIST: &'static [&'static str] = &[
-    "UnityCrashHandler64.exe"
-];
 
 
 
@@ -98,36 +92,10 @@ impl Game {
 
     pub async fn start(&mut self) -> Result<(), Box<dyn Error>> {
 
-        fn search_exe(path: PathBuf) -> Result<Option<PathBuf>, Box<dyn Error>> {
-            let mut queue: Vec<PathBuf> = vec![ path ];
-        
-            while queue.len() > 0 {
-                let path = queue.remove(0);
-        
-                if SEARCH_BLACKLIST.iter().any(|blacklisted| {
-                    path.to_str().unwrap().ends_with(blacklisted)
-                }) {
-                    continue;
-                }
-        
-                if path.is_dir() {
-                    for entry in path.read_dir()? {
-                        queue.push(entry?.path());
-                    }
-                } else if path.is_file() {
-                    if path.extension().is_some() && path.extension().unwrap() == "exe" {
-                        return Ok(Some(path.to_owned()));
-                    }
-                }
-            }
-        
-            Ok(None)
-        }
-
         let mut search_path = PathBuf::from(&self.config.base_dir);
         search_path.push("games");
         search_path.push(&self.directory);
-        let executable_path = search_exe(search_path)?;
+        let executable_path = find_executable(search_path)?;
 
         if executable_path.is_some() {
             let full_path = fs::canonicalize(executable_path.unwrap()).await?;
@@ -142,3 +110,84 @@ impl Game {
     }
 
 }
+
+
+
+
+
+static SEARCH_BLACKLIST: &'static [&'static str] = &[
+    "UnityCrashHandler64.exe",
+    "UnityCrashHandler32.exe" // Don't actually know if this exists but include it just incase.
+];
+
+fn is_executable_blacklisted(path: &PathBuf) -> bool {
+    SEARCH_BLACKLIST.iter().any(|blacklisted| {
+        path.to_str().unwrap().ends_with(blacklisted)
+    })
+}
+
+fn find_executable_parent(path: PathBuf) -> Result<Option<PathBuf>, Box<dyn Error>> {
+    let mut queue: Vec<PathBuf> = vec![ path ];
+
+    while queue.len() > 0 {
+        let path = queue.remove(0);
+
+        if is_executable_blacklisted(&path) {
+            continue;
+        }
+
+        if path.is_dir() {
+            for entry in path.read_dir()? {
+                queue.push(entry?.path());
+            }
+        } else if path.is_file() {
+            if path.extension().is_some() && path.extension().unwrap() == "exe" {
+                return Ok(match path.parent() {
+                    Some(parent) => Some(PathBuf::from(parent)),
+                    None => None,
+                });
+            }
+        }
+    }
+
+    Ok(None)
+}
+
+fn find_executable(path: PathBuf) -> Result<Option<PathBuf>, Box<dyn Error>> {
+    match find_executable_parent(path)? {
+        Some(executable_dir) => {
+
+            let mut executables: Vec<DirEntry> = executable_dir.read_dir()?
+                .filter(|item| {
+                    match item {
+                        Ok(item) => {
+                            let path = item.path();
+                            return path.is_file() && 
+                                !is_executable_blacklisted(&path) &&
+                                (path.extension().is_some() && path.extension().unwrap() == "exe");
+                        },
+                        Err(_) => false,
+                    }
+                })
+                .map(|item| item.unwrap())
+                .collect::<Vec<DirEntry>>();
+
+            executables.sort_by(|a, b| {
+                // Use x64 executables over x32
+                if a.file_name().to_str().unwrap().contains("64") { return std::cmp::Ordering::Greater; }
+                if b.file_name().to_str().unwrap().contains("64") { return std::cmp::Ordering::Less; }
+                
+                // Otherwise biggest is probably the correct executable
+                let a_meta = a.metadata().unwrap();
+                let b_meta = b.metadata().unwrap();
+                a_meta.len().cmp(&b_meta.len())
+            });
+
+            Ok(Some(executables[0].path()))
+
+        },
+        None => Ok(None)
+    }
+}
+
+
