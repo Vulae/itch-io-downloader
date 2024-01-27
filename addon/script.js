094b46e1f5bcc33dcdab6795cfd0e98c07b660d4
Vulae <potatocrazy42069@gmail.com>
2024-01-26 19:47:46 -0600

    fix play button icon not resizing

--- addon/script.js
+++ addon/script.js
@@ -36,7 +36,11 @@ async function displayPlayButton(element, gameInfo) {
     element.insertAdjacentHTML('beforeend', `
         <li>
             <a class="action_btn" href="itch-io-downloader://play/${gameInfo.id}">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" x2="12" y1="15" y2="3"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svgicon lucide lucide-download">
+                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
+                    <polyline points="7 10 12 15 17 10"/>
+                    <line x1="12" x2="12" y1="15" y2="3"/>
+                </svg>
                 Download & Play
             </a>
         </li>
